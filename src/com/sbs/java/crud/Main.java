6b95abc100eb6d669a892083350ae206d49a9bdc
jonggoo82 <jonggoo82@naver.com>
2021-05-01 16:04:50 +0900

    스캐너

---
+++ src/com/sbs/java/crud/Main.java
@@ -0,0 +1,23 @@
+package com.sbs.java.crud;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.printf("명령어 : ");
+		String command = sc.nextLine();
+
+		System.out.println("입력된 명령어 : " + command);
+
+		System.out.printf("명령어 : ");
+		int num = sc.nextInt();
+		System.out.println("입력된 명령어 : " + num);
+		sc.close();
+
+		System.out.println("==프로그램 끝==");
+	}
+}
