c6c5786d84804ae768f6c68234fb00bee34c4559
jonggoo82 <jonggoo82@naver.com>
2021-05-01 17:14:18 +0900

    게시물 작성 기능 구현중

--- src/com/sbs/java/crud/Main.java
+++ src/com/sbs/java/crud/Main.java
@@ -4,20 +4,45 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+
 		System.out.println("==프로그램 시작==");
 
 		Scanner sc = new Scanner(System.in);
 
-		System.out.printf("명령어 : ");
-		String command = sc.nextLine();
+		int lastArticleId = 0;
+		while (true) {
+			System.out.printf("명령어 : ");
+			String command = sc.nextLine().trim();
 
-		System.out.println("입력된 명령어 : " + command);
+			if (command.length()==0) {
+				System.out.println("명령어를 입력해 주세요.");
+				continue;
+			}
+			if (command.equals("system exit")) {
+				break;
+			}
 
-		System.out.printf("명령어 : ");
-		int num = sc.nextInt();
-		System.out.println("입력된 명령어 : " + num);
+			if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목: ");
+				String title = sc.nextLine();
+
+				System.out.printf("내용: ");
+				String body = sc.nextLine();
+
+				System.out.printf("%d번 글이 생성 되었습니다.\n", id);
+			}else if(command.equals("article list")){
+				System.out.println("게시물이 없습니다.");				
+			}
+			else {
+				System.out.println(command + "은(는) 존재하지 않는 명령어 입니다.");
+			}
+
+		}
+
 		sc.close();
-
+		
 		System.out.println("==프로그램 끝==");
 	}
 }
